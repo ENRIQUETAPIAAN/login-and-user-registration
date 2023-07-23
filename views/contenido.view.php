0ca1cf87bbb409b6760b07ab2afc2c1b495f3ab0
enriquetapiaan <tapiaa.antunes@outlook.com>
2023-07-23 10:46:02 -0600

    Creación de archivos iniciales

---
+++ views/contenido.view.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, 
+    minimum-scale=1.0, maximum-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/estilos.css">
+    <title>Contenido protegido</title>
+</head>
+<body>
+    <div class="contenedor">
+        <h1 class="titulo">Contenido del sitio</h1>
+        <a href="cerrar.php">Cerrar Sesión</a>
+        <div class="contenido">
+            <article>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam harum est sunt non necessitatibus modi quo veritatis illum, a vero? Velit perferendis ex atque repellendus quae suscipit quaerat voluptates voluptatem.</p>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae voluptas at optio animi voluptatum assumenda reiciendis quis nostrum fugiat totam. Nostrum dignissimos velit omnis adipisci reprehenderit repellat quasi sequi ratione?</p>
+            </article>
+        </div>
+    </div>
+</body>
+</html>
