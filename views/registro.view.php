4dd7ebaf17575b55f28c7e357fe0b11de15ddde4
enriquetapiaan <tapiaa.antunes@outlook.com>
2023-08-20 18:51:48 -0600

    agregue la pagina de registro y un poco de logica

---
+++ views/registro.view.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, 
+    minimum-scale=1.0, maximum-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/estilos.css">
+    <title>Registro</title>
+</head>
+<body>
+    <div class="contenedor">
+        <h1 class="titulo">Contenido del sitio</h1>
+        <hr class="border">
+        <div class="contenido">
+           
+        </div>
+    </div>
+</body>
+</html>
