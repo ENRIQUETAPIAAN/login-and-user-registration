bf450f0c1c723bff8d28aa5527f1b75d659f70d3
enriquetapiaan <tapiaa.antunes@outlook.com>
2023-08-22 22:47:11 -0600

    se agrego logica a registro y nuevo login

--- views/login.view.php
+++ views/login.view.php
@@ -9,28 +9,25 @@
     <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/07702d4e25.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="css/estilos.css">
-    <title>Registro</title>
+    <title>Iniciar Sesión</title>
 </head>
 <body>
     <div class="contenedor">
-        <h1 class="titulo">Registro</h1>
+        <h1 class="titulo">Iniciar Sesión</h1>
         <hr class="border">
         <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="POST" class="formulario" name="login">
             <div class="form-group">
                 <i class="icono izquierda fa fa-user"></i><input type="text" name="usuario" class="usuario" placeholder="Usuario:">
             </div>
             <div class="form-group">
-                <i class="icono izquierda fa fa-lock"></i><input type="password" name="password" class="password" placeholder="Contraseña:">
-            </div>
-            <div class="form-group">
-                <i class="icono izquierda fa fa-lock"></i><input type="password" name="password2" class="password_btn" placeholder="Repetir Contraseña:">
+                <i class="icono izquierda fa fa-lock"></i><input type="password" name="password" class="password_btn" placeholder="Contraseña:">
                 <i class="submit-btn fa fa-arrow-right" onclick="login.submit()"></i>
             </div>
         </form>
 
         <p class="texto-registrate">
-            ¿ Ya tienes cuenta ?
-            <a href="login.php">Iniciar Sesión</a>
+            ¿ Aún no tienes cuenta ?
+            <a href="registro.php">Registrate</a>
         </p>
 
     </div>
